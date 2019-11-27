8fd39e6780968ff457e8bb56277a8de03dc05702
oboutrol <octave.boutrolle@gmail.com>
2019-11-27 15:41:25 +0000

    version live

--- embedded.py
+++ embedded.py
@@ -1,20 +1,24 @@
+import subprocess
 from matplotlib import *
 import matplotlib.pyplot as plt
 import numpy as np
 from subprocess import *
 #rom subprocess import run
-ok = subprocess.call('./dec')
-file = open("tmp", "r")
-string = file.read()
-file.close()
-data = string.split()
-integ = []
-for k in data:
-	j = int(k)
-	#if j == 0: j = 6000
-	integ.append(j)
-x = np.linspace(-0.5244, 3.6644, len(integ))
+
+def print_map(flag):
+    ok = subprocess.call('./dec')
+    file = open("tmp", "r")
+    string = file.read()
+    file.close()
+    data = string.split()
+    integ = []
+    for k in data:
+        j = int(k)
+        integ.append(j)
+    	    #if j == 0: j = 6000
+    x = np.linspace(-0.5244, 3.6644, len(integ))
 #plt.plot(data) #linear mode
 #plt.show() #linear mode
-plt.polar(x, integ)
-plt.show()
+    plt.polar(x, integ)
+    if (flag == 1):
+        plt.show()
--- usbtest.py
+++ usbtest.py
@@ -1,8 +1,10 @@
 import serial
+import subprocess
+from embedded import print_map
+from subprocess import *
+import matplotlib.pyplot as plt
 
 serialLid = serial.Serial('/dev/ttyACM0', 9600, timeout=0.50)
-
-print (hex (ord ('V')))
 
 def write_cmd(string):
     if (type(string) == type('thisisastring')):
@@ -13,13 +15,14 @@ def write_cmd(string):
             command.append( ord(char))
         serialLid.write(command)
 
-def read_cmd():
+def read_cmd(flag):
     f = open("encryptx.txt", 'w+')
     a = 1
     while a:    
         while serialLid.in_waiting:
             data = serialLid.readline()
-            print (data)
+            if (flag == 1):
+                print (data)
             f.write(data.decode())
             a = 0
 
@@ -28,6 +31,18 @@ while True:
     string = input('would you command this lidar ?\n')
     if (string == 'last'):
         print (last)
+    elif (string == 'print'):
+        print_map(1)
+    elif (string == 'auto'):
+        string = last[1]
+        for k in range(1000):
+            write_cmd(string)
+            read_cmd(0)
+            plt.clf()
+            print_map(0)
+            plt.pause(0.001)
+        plt.show()
+        #subprocess.call('python3 embedded.py')
     else:
         if (string[0] >= '0' and string[0] <= '9'):
             string = last[int(string[0]) % len(last)]
@@ -35,4 +50,4 @@ while True:
             string = string + '\n'
         last.append(string)
         write_cmd(string)
-        read_cmd()
+        read_cmd(1)
