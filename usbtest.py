20e0762c532720abbc850f0cdb2bf6a188f83d44
Octave <octave.boutrolle@gmail.com>
2019-11-27 20:46:11 +0100

    adding sketch for scan

--- usbtest.py
+++ usbtest.py
@@ -2,6 +2,7 @@ import serial
 import subprocess
 from embedded import print_map
 from subprocess import *
+import numpy as np
 import matplotlib.pyplot as plt
 
 serialLid = serial.Serial('/dev/ttyACM0', 9600, timeout=0.50)
@@ -26,6 +27,26 @@ def read_cmd(flag):
             f.write(data.decode())
             a = 0
 
+def improve(old_data, nbr)
+    ok = subprocess.call('./dec')
+    file = open("tmp", "r")
+    string = file.read()
+    file.close()
+    data = string.split()
+    if (nbr == 0 or old_data == []):
+        new_data = []
+    	for k in data:
+            j = int(k)
+	    new_data.append(j)
+	return (new_data)
+    else:
+        new_data = []
+    	for nb in range(data)
+	    j = int(data[nb])
+	    i = int(old_data[nb])
+	    new_data.append((i * (nbr - 1) + j) / nbr)
+	return (new_data)
+  
 last = ['MS004407250103\n', 'GD0044072501\n']
 while True:
     string = input('would you command this lidar ?\n')
@@ -43,6 +64,16 @@ while True:
             plt.pause(0.001)
         plt.show()
         #subprocess.call('python3 embedded.py')
+    elif (string == 'scan'):
+	string = last[1]
+	number = input('combien de tours de scan ?')
+	for k in range(int(number)):
+            write_cmd(string)
+            read_cmd(0)
+	    old_data = improve(old_data, k)
+    	x = np.linspace(-0.5244, 3.6644, len(old_data))
+	plt.polar(x, old_data)
+	plt.show()
     else:
         if (string[0] >= '0' and string[0] <= '9'):
             string = last[int(string[0]) % len(last)]
