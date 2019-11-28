328c059f76b0d3c7e18c84ba0399b2d3d500ddc4
oboutrol <octave.boutrolle@gmail.com>
2019-11-28 10:01:08 +0000

    fixed scan, tested, best data with scan 10-30

--- usbtest.py
+++ usbtest.py
@@ -35,17 +35,17 @@ def improve(old_data, nbr):
     data = string.split()
     if (nbr == 0 or old_data == []):
         new_data = []
-    	for k in data:
+        for k in data:
             j = int(k)
-	    new_data.append(j)
-	return (new_data)
+            new_data.append(j)
+        return (new_data)
     else:
         new_data = []
-    	for nb in range(data)
-	    j = int(data[nb])
-	    i = int(old_data[nb])
-	    new_data.append((i * (nbr - 1) + j) / nbr)
-	return (new_data)
+        for nb in range(len(data)):
+            j = int(data[nb])
+            i = int(old_data[nb])
+            new_data.append((i * (nbr - 1) + j) / nbr)
+        return (new_data)
 
 def do_help(last):
     print ('Type some of the following key word or a raw command (PP, VV, GD..., ...) for the lidar:\n')
@@ -60,11 +60,10 @@ def do_print(last):
 def do_last(last):
     print (last)
     
-  
 def do_auto(last):
     string = last[1]
     for k in range(1000):
-    	write_cmd(string)
+        write_cmd(string)
         read_cmd(0)
         plt.clf()
         print_map(0)
@@ -74,27 +73,28 @@ def do_auto(last):
 def do_scan(last):
     string = last[1]
     number = input('combien de tours de scan ?')
+    old_data = []
     for k in range(int(number)):
-    	write_cmd(string)
-	read_cmd(0)
-	old_data = improve(old_data, k)
+        write_cmd(string)
+        read_cmd(0)
+        old_data = improve(old_data, k)
     x = np.linspace(-0.5244, 3.6644, len(old_data))
     plt.polar(x, old_data)
     plt.show()
-    
+
 last = ['MS004407250103\n', 'GD0044072501\n']
 while True:
     string = input('what would you wish to accomplish ? type help for... help !\n')
     if (string == 'help'):
     	do_help(last)
     elif (string == 'last'):
-    	do_last(last)
+        do_last(last)
     elif (string == 'print'):
-    	do_print(last)
+        do_print(last)
     elif (string == 'auto'):
-    	do_auto(last)
+        do_auto(last)
     elif (string == 'scan'):
-    	do_auto(last)
+        do_scan(last)
     else:
         if (string[0] >= '0' and string[0] <= '9'):
             string = last[int(string[0]) % len(last)]
